b9547b0877e6ec8e65af3c171e39026f57fd1e9f
Crush-0 <111955144+Crush-0@users.noreply.github.com>
2023-06-28 16:27:20 +0800

    [update]

---
+++ delete_folder_server.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set folderPath=C:\Users\ChengYao\Desktop\train\github-action\test\server
+
+if exist "%folderPath%" (
+    echo 文件夹存在
+    rmdir /s /q "%folderPath%"
+    echo 文件夹已成功删除
+) else (
+    echo 文件夹不存在
+    exit 1
+)
