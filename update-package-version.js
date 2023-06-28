07d6972baa9ca87e0b3fc1944b2e9b2dfd432596
Crush-0 <111955144+Crush-0@users.noreply.github.com>
2023-06-28 17:37:41 +0800

    Update update-package-version.js

--- update-package-version.js
+++ update-package-version.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 // 读取 RRTV-client 目录下的 package.json 文件
-const clientPackageJsonPath = '../test/RRTV-client/package.json';
+const clientPackageJsonPath = './test/RRTV-client/package.json';
 const clientPackageJson = JSON.parse(fs.readFileSync(clientPackageJsonPath));
 
 // 读取主目录下的 package.json 文件
-const mainPackageJsonPath = '../test/package.json';
+const mainPackageJsonPath = './test/package.json';
 const mainPackageJson = JSON.parse(fs.readFileSync(mainPackageJsonPath));
 
 // 更新主目录下的 package.json 中的 version 值
